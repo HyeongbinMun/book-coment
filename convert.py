1ab989a754c3981317619cb178d4325b21111e2e
ruinarts <m537499099@gmail.com>
2022-12-28 15:12:38 +0900

    - convert type update, test txt update

--- convert.py
+++ convert.py
@@ -2,7 +2,10 @@ import os
 import pandas as pd
 import argparse
 
-def csvtotext(data_path):
+def csvtotext(args):
+    data_path = args.data_path
+    data_type = args.data_type
+
     file_list = os.listdir(data_path)
     for file in file_list:
         file_name, _ = file.split('.')
@@ -11,21 +14,31 @@ def csvtotext(data_path):
 
         save_path = data_path + file_name + '.txt'
         f = open(save_path, 'w')
-        subject = 'id' + '\t' + 'document' + '\t' + 'label' + '\n'
-        f.write(subject)
 
         for i in range(len(data_list['Content'])):
-            if data_list['label'][i] == 'N': label = '0'
-            elif data_list['label'][i] == 'P': label = '1'
-            else: label = '2'
-            content = data_list['Content'][i].replace('\n', ' ')
-            data = str(data_list['ID'][i]) + '\t' + content + '\t' + label + '\n'
-            f.write(data)
+            if data_type == 'train':
+                subject = 'id' + '\t' + 'document' + '\t' + 'label' + '\n'
+                f.write(subject)
+
+                if data_list['label'][i] == 'N': label = '0'
+                elif data_list['label'][i] == 'P': label = '1'
+                else: label = '2'
+                content = data_list['Content'][i].replace('\n', ' ')
+                data = str(data_list['ID'][i]) + '\t' + content + '\t' + label + '\n'
+                f.write(data)
+            else:
+                subject = 'id' + '\t' + 'document' + '\n'
+                f.write(subject)
+
+                content = data_list['Content'][i].replace('\n', ' ')
+                data = str(data_list['ID'][i]) + '\t' + content + '\t' + '\n'
+                f.write(data)
         f.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-data_path', '--data_path', type=str, default='/workspace/data/')
+    parser.add_argument('--data_path', type=str, default='/workspace/data/train.xlsx')
+    parser.add_argument('--data_type', type=str, default='test', help='train or test')
     args = parser.parse_args()
 
-    csvtotext(args.data_path)
+    csvtotext(args)
